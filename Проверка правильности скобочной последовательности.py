2637ee247733c8beab6039a5b4f1b8ab21cafd01
August <bocheskov@bk.ru>
2025-04-27 17:25:57 +0300

    Задание №1.

--- Проверка правильности скобочной последовательности.py
+++ Проверка правильности скобочной последовательности.py
@@ -1,1 +1,46 @@
-Проверка
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise IndexError("Стек пуст")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Стек пуст")
+
+
+def is_valid_parentheses(s):
+    stack = Stack()
+    matching_pairs = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in '({[':
+            stack.push(char)
+        elif char in ')}]':
+            if stack.is_empty():
+                return False
+            top_element = stack.pop()
+            if top_element != matching_pairs.get(char):
+                return False
+
+    return stack.is_empty()
+
+
+parentheses_sequence = input("Введите скобочную последовательность: ")
+
+if is_valid_parentheses(parentheses_sequence):
+    print("Строка является правильной скобочной последовательностью.")
+else:
+    print("Строка является не правильной скобочной последовательностью.")
